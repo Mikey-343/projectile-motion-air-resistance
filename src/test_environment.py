9fd11b990ef7c86718b0e74b504c8a324d4e6b95
Mikey-343 <mikehan343@gmail.com>
2026-09-12 16:22:37 +0530

    Set up Python environment and dependencies

---
+++ src/test_environment.py
@@ -0,0 +1,8 @@
+import numpy
+import matplotlib
+import scipy
+
+print("Python environment is working.")
+print(f"NumPy      : {numpy.__version__}")
+print(f"Matplotlib : {matplotlib.__version__}")
+print(f"SciPy      : {scipy.__version__}")
